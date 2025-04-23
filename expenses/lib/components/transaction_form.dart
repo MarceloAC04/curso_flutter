05fddd39b6d14230f47a6035666b7fa9c7481d4d
Marcelo Cardoso- AfixCode <marcelo.cardoso@afixcode.com.br>
2025-04-23 16:03:14 -0300

    Finalizado as funcionalidades

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 class TransactionForm extends StatefulWidget {
-
-  final void Function(String, double) onSubmit;
+  final void Function(String, double, DateTime) onSubmit;
 
   TransactionForm(this.onSubmit);
 
@@ -11,25 +10,51 @@ class TransactionForm extends StatefulWidget {
 }
 
 class _TransactionFormState extends State<TransactionForm> {
-  final titleController = TextEditingController();
-
-  final valueController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
 
   _submitForm() {
-      final title = titleController.text;
-      final value = double.tryParse(valueController.text) ?? 0.0;
+      final title = _titleController.text;
+      final value = double.tryParse(_valueController.text) ?? 0.0;
 
-      if (title.isEmpty || value <= 0) {
+      if (title.isEmpty || value <= 0 || _selectedDate == null) {
         return;
       }
-      widget.onSubmit(title, value);
+      widget.onSubmit(title, value, _selectedDate);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+        context: context,
+        firstDate: DateTime(2025),
+        initialDate: DateTime.now(),
+        lastDate: DateTime.now()
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+      _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style =
     ElevatedButton.styleFrom(
-        foregroundColor: Colors.purple
+        foregroundColor: Colors.white,
+      padding: EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 20
+      )
+    );
+    final ButtonStyle textButtonStyle =
+    ElevatedButton.styleFrom(
+        foregroundColor: Theme.of(context).colorScheme.primary
+
     );
     return Card(
       elevation: 5,
@@ -38,15 +63,35 @@ class _TransactionFormState extends State<TransactionForm> {
         child: Column(
           children: <Widget>[
             TextField(
-                controller: titleController,
+                controller: _titleController,
                 // Usa se _ quando se quer ignorar o parâmetro.
                 onSubmitted: (_) => _submitForm(),
                 decoration: InputDecoration(labelText: 'Título')),
             TextField(
-                controller: valueController,
+                controller: _valueController,
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
                 onSubmitted: (_) => _submitForm(),
                 decoration: InputDecoration(labelText: 'Valor (R\$)')),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                        _selectedDate == null ? 'Nenhuma data selecionada!'
+                    : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate!)}'),
+                  ),
+                  TextButton(
+                    style: textButtonStyle,
+                      onPressed: _showDatePicker,
+                      child: Text('Selecionar Data',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),),
+                  )
+                ],
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
 import 'package:expenses/models/transaction.dart';
@@ -19,9 +20,22 @@ class ExpensesApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: false,
         fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         appBarTheme: AppBarTheme(
           backgroundColor: Colors.purple,
           foregroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.amber,
@@ -39,27 +53,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de Corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
+  final List<Transaction> _transactions = [];
 
-  _addTransaction(String title, double value) {
+  List<Transaction> get _recentTransaction {
+    return _transactions.where((tr) {
+      return tr.date!.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
+  _addTransaction(String title, double value, DateTime date) {
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
       title: title,
       value: value,
-      date: DateTime.now(),
+      date: date,
     );
 
     setState(() {
@@ -78,32 +85,42 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  _removeTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text('Despesas Pessoais'),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () => _openTransactionFormModal(context),
+          icon: Icon(Icons.add),
+        ),
+      ],
+    );
+    final availabeHeight =
+        MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () => _openTransactionFormModal(context),
-            icon: Icon(Icons.add),
-          ),
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            SizedBox(
-              child: Container(
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Gráfico'),
-                  elevation: 5,
-                ),
-              ),
+            Container(
+              height: availabeHeight * 0.6,
+              child: Chart(_recentTransaction),
             ),
-            TransactionList(_transactions),
+            Container(
+              height: availabeHeight * 0.6,
+              child: TransactionList(_transactions, _removeTransaction),
+            ),
           ],
         ),
       ),
