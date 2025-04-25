1c25be04bc1ac015dcc78eee41d48a1ba2f08581
Marcelo Araujo Cardoso <marcelocardoso020304@gmail.com>
2025-04-25 16:02:27 -0300

    Iniciado o projeto Meals (Seção 7)

---
+++ meals/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+import 'screens/categories_screen.dart';
+import 'package:meals/utils/app_routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 20,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+          seedColor: Colors.amber,
+        ),
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+      },
+    );
+  }
+}
