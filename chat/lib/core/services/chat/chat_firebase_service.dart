28165b025af44b1516cd3ff68cca2f19c27d8bfc
Marcelo Araujo Cardoso <marcelocardoso020304@gmail.com>
2025-05-13 16:00:36 -0300

    Finalizado o projeto chat e inicaido a seção código nativo

---
+++ chat/lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:chat/core/services/chat/chat_service.dart';
+import '../../models/chat_massage.dart';
+import '../../models/chat_user.dart';
+
+class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [
+    // ChatMessage(
+    //   id: '1',
+    //   text: 'Bom dia',
+    //   createdAt: DateTime.now(),
+    //   userId: '123',
+    //   userName: 'Bia',
+    //   userImageURL: 'assets/images/avatar.png',
+    // ),
+    // ChatMessage(
+    //   id: '2',
+    //   text: 'Bom dia. Teremos reunião hoje?',
+    //   createdAt: DateTime.now(),
+    //   userId: '456',
+    //   userName: 'Ana',
+    //   userImageURL: 'assets/images/avatar.png',
+    // ),
+    // ChatMessage(
+    //   id: '1',
+    //   text: 'Sim. Pode ser agora!',
+    //   createdAt: DateTime.now(),
+    //   userId: '123',
+    //   userName: 'Bia',
+    //   userImageURL: 'assets/images/avatar.png',
+    // ),
+  ];
+  static MultiStreamController<List<ChatMessage>>? _controller;
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+    controller.add(_msgs);
+  });
+
+  Stream<List<ChatMessage>> messageStream() {
+    return _msgsStream;
+  }
+
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final newMessage = ChatMessage(
+      id: Random().nextDouble().toString(),
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageURL: user.imageUrl,
+    );
+
+    _msgs.add(newMessage);
+    _controller?.add(_msgs.reversed.toList());
+    return newMessage;
+  }
+}
